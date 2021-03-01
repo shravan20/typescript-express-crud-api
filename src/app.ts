76ece32ead9e8fb2514be9066d0157202f15ee12
shravan20 <mrshravankumarb@gmail.com>
2021-03-01 17:57:24 +0530

    [ removed swaggerSetup  ] #015 bugfix | Commented the swaggerSetup [temporarily]

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,6 @@ import { Routes } from '@app/routes';
 import expressRequestId from 'express-request-id';
 const RequestId = expressRequestId();
 import * as bodyParser from 'body-parser';
-// import swaggerJSDoc from 'swagger-jsdoc';
-// import swaggerUi from 'swagger-ui-express';
 
 
 export default class App {
@@ -26,23 +24,5 @@ export default class App {
     this.app.use(morgan('dev'));
   }
 
- /*  private swaggerSetup(): void {
-  
-    const swaggerDefinition = {
-      info: {
-          title: 'TEST - Swagger API',
-          version: '1.0.0',
-          description: 'API documentation',
-      },
-      host: process.env.APP_HOST,
-    };
-    const options = { */
-        // swaggerDefinition,
-        // apis: ['**/*.ts'],
-    // };
-    // const swaggerSpec = swaggerJSDoc(options);
-    // this.app.use('/test/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-  
-  // }
 
 }
