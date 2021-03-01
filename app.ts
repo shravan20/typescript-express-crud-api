62af420dce660d1ba2137bdb1f4fb3fd2f33c76c
shravan20 <mrshravankumarb@gmail.com>
2021-03-01 11:57:45 +0530

    Updated PACKAGES

--- app.ts
+++ app.ts
@@ -1,9 +1,16 @@
 import morgan from 'morgan';
 import express from "express";
 import userRouter  from "./src/routes/user";
+import expressRequestId from 'express-request-id';
+const RequestId = expressRequestId();
+
 
 let app = express();
 app.use(morgan('dev'));
+app.use((req,res,next) => {
+    console.log(RequestId);
+    next()
+});
 
 
 app.use('/user', userRouter)
