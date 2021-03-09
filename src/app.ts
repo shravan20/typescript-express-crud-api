f63f7c8e4c2b9875dcdef15d2924d4380b1601dc
shravan20 <mrshravankumarb@gmail.com>
2021-03-09 17:30:34 +0530

    :bug: Fixing Swagger

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ const RequestId = expressRequestId();
 import * as bodyParser from 'body-parser';
 import swaggerJSDoc from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
-import swaggerSpec from '../swaggerDef';
+// import options from './swaggerDef';
 
 export default class App {
 
@@ -17,6 +17,7 @@ export default class App {
     this.app = express();
     this.config();
     this.routes.routes(this.app);
+    this.swaggerSetup();
   }
 
   private config(): void {
@@ -26,6 +27,18 @@ export default class App {
 
   private swaggerSetup(): void {
 
+    let options = {
+      info: {
+          // API informations (required)
+          title: 'Node-Typescript API', // Title (required)
+          version: '1.0.0', // Version (required)
+          description: 'A sample API', // Description (optional)
+      },
+      host: 'http://localhost:4040/',
+      apis: ['./controller/user.ts']
+    };
+    console.log(options);
+    const swaggerSpec = swaggerJSDoc(options);
     this.app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 }
 
--- src/swaggerDef.ts
+++ src/swaggerDef.ts
@@ -1,22 +1,20 @@
 import * as path from 'path';
-import swaggerJSDoc from 'swagger-jsdoc';
+// import swaggerJSDoc from 'swagger-jsdoc';
 
 let options = {
-    openapi: '3.0.0',
     info: {
         // API informations (required)
         title: 'Node-Typescript API', // Title (required)
         version: '1.0.0', // Version (required)
         description: 'A sample API', // Description (optional)
     },
-    servers: [
+    host: 'localhost:4040',
+    basePath:'/',
+    /* servers: [
         { url: 'http://localhost:4040' }
-    ],
-    apis: [path.join(__dirname, './src/controller/user.ts')]
+    ], */
+    apis: ['./controller/*.ts']
 };
 
-const swaggerSpec = swaggerJSDoc(options);
 
-export = {
-  swaggerSpec
-};
+export = options;
